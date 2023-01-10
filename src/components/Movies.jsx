8e0c53b4734b7b5082b070fa82044db593c8914e
adheelali <adheelali2@gmail.com>
2023-01-10 04:32:45 -0800

    ..

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import './movies.css'
+import './styles/movies.css'
 
 function Movies({ data }) {
   return (
     <main>
       <div className="movie__list">
-        {data?.Search.map((data) => (
+        {data?.Search?.map((data) => (
           <div className="movie__wrapper">
             <figure className="movie__img--wrapper">
               <img src={data.Poster} alt="" />
--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -4,7 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { movieTextState } from "../../atoms/Atom";
-import Movies from "./UI/Movies";
+import Movies from "../components/Movies";
 
 function Browse() {
   // API request for once
